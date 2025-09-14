dba01f85cd50c9684c4edce5c5fd1749161d3b46
Dennis <dennis-farris@hotmail.com>
2025-09-14 14:08:06 -0400

    added ability to delete tasks, check archive permissions before allowing user to archive/unarchive tasks

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -19,3 +19,46 @@ export const addTask = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+export const editTask = async (req, res) => {
+  if (!req.task) return res.status(403).json({ message: "unauthorized" });
+  if (!req.body) return res.status(400).json({ message: "body missing" });
+
+  if (
+    //ensures user can make edits to archive if they are attempting it
+    req.body.status &&
+    ((req.body.status === "archive" && req.task.status !== "archive") ||
+      (req.body.status !== "archive" && req.task.status === "archive"))
+  ) {
+    const user = req.project.users.find((collab) =>
+      collab.user.equals(req.user._id)
+    );
+    if (!user.permissions.includes("archiveTask"))
+      return res.status(403).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const task = Object.assign(req.task, req.body);
+
+    await task.save();
+
+    res.json(task);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  if (!req.task) return res.status(403).json({ message: "unauthorized" });
+  if (!req.body) return res.status(400).json({ message: "body missing" });
+
+  try {
+    await req.task.deleteOne();
+
+    res.json({ message: "Task successfully deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,7 +1,8 @@
 import e from "express";
 import { contentMiddleware } from "../middleware/middleware";
-import { addTask, getTask } from "../controllers/taskControllers";
+import { addTask, deleteTask, editTask, getTask } from "../controllers/taskControllers";
 import Project from "../models/Project";
+import Task from "../models/Task"
 const router = e.Router();
 
 //api/projects/:projectId/tasks/:taskId
@@ -9,5 +10,7 @@ router.post("/:taskId", contentMiddleware(Project, "users", "addTask"), addTask)
 
 router.use(contentMiddleware(Project, "users", "getProject"))
 router.get("/:taskId", getTask);
+router.put("/:taskId", contentMiddleware(Task, "project", "editTask"), editTask)
+router.delete(":/taskId", contentMiddleware(Task, "project", "deleteTask"), deleteTask)
 
 export default router;
