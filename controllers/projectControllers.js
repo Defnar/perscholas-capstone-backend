212f5cf8d75b887c5c51ed226e03e75672e9af8b
Dennis <dennis-farris@hotmail.com>
2025-09-14 12:43:21 -0400

    created logic and route for getting private projects

--- controllers/projectControllers.js
+++ controllers/projectControllers.js
@@ -1,7 +1,6 @@
 import Project from "../models/Project";
 
 export const getPublicProjects = async (req, res) => {
-
   const sortBy = req.query.sortBy || "name";
   const sortOrder = req.query.sortOrder || 1;
   const title = req.query.sortAuthor || "";
@@ -10,6 +9,7 @@ export const getPublicProjects = async (req, res) => {
   const page = Number(req.query.page) || 1;
 
   try {
+    //this was a pain to figure out.  hoping it works 👀
     const projects = await Project.aggregate()
       .match({
         private: false,
@@ -35,8 +35,31 @@ export const getPublicProjects = async (req, res) => {
       .skip((page - 1) * pageSize)
       .limit(pageSize);
 
-      res.json(projects);
+    res.json(projects);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
 
+export const getPrivateProjects = async (req, res) => {
+  const sortBy = req.query.sortBy || "name";
+  const sortOrder = req.query.sortOrder || 1;
+  const title = req.query.sortAuthor || "";
+  const pageSize = Number(req.query.pageSize) || 10;
+  const page = Number(req.query.page) || 1;
+  const userId = req.user._id;
+
+  try {
+    const projects = await Project.find({
+      title: {
+        $regex: title,
+        $options: "i",
+      },
+      users: {
+        $in: [userId],
+      },
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal server error" });
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,8 +1,11 @@
 import e from "express";
-import { getPublicProjects } from "../controllers/projectControllers";
+import { getPrivateProjects, getPublicProjects } from "../controllers/projectControllers";
 const router = e.Router();
 
 
 //api/projects
 
 router.get("/", getPublicProjects);
+router.get("/private", getPrivateProjects);
+
+
