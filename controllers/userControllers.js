1bc77984e57dc4660c20deae54d2e6b2465bf00e
Dennis <dennis-farris@hotmail.com>
2025-09-14 14:13:29 -0400

    attached user to register user return statement

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -30,7 +30,7 @@ export const register = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    res.status(201).json({ message: "user successfully created" });
+    res.status(201).json({ message: "user successfully created", user });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
