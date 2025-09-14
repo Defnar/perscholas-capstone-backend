672eadae0386e6dba45d1f8d817c670abeb9a611
Dennis <dennis-farris@hotmail.com>
2025-09-14 14:09:48 -0400

    added cors, finished out server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import "dotenv/config";
 import e from "express";
 import db from "./config/connection";
+import indexRoutes from "./routes/index"
+import cors from "cors";
 
 const app = e();
 const port = process.env.PORT || 8000;
 
 app.use(e.json());
 
-app.use("/api", /*routes go here*/);
+app.use(cors());
+
+app.use("/api", indexRoutes);
 
 db.once("connect", () => {
     app.listen(port, () => console.log(`server up at http://localhost:${port}`))
